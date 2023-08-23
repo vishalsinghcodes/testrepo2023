70893781e153509fe79b28f867afd813b769a7e2
Vishal Singh <vishalsingh164@gmail.com>
2023-08-24 01:02:30 +0530

    This is c04 by PY

--- src/gitPracticePackage/PracticeClass02.java
+++ src/gitPracticePackage/PracticeClass02.java
@@ -4,6 +4,8 @@ public class PracticeClass02 {
 	
 	public static void main(String args[]) {
 		System.out.println("This class is created by Person X");
+		
+		System.out.println("This something added by PY in new class");
 	}
 
 }
