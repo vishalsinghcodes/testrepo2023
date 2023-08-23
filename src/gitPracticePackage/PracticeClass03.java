260d8df864baf53cbd5ed7028dad586601df09f4
Vishal Singh <vishalsingh164@gmail.com>
2023-08-24 01:22:01 +0530

    This is c06 by PX in Branch01ByPx

---
+++ src/gitPracticePackage/PracticeClass03.java
@@ -0,0 +1,17 @@
+package gitPracticePackage;
+
+public class PracticeClass03 {
+	
+	public static void main(String args[]) {
+		System.out.println("This class is added first in Branch01ByPx ");
+		
+		System.out.println("This class is added first in Branch01ByPx ");
+		
+		System.out.println("This class is added first in Branch01ByPx ");
+		
+		System.out.println("This class is added first in Branch01ByPx ");
+		
+		System.out.println("This class is added first in Branch01ByPx ");
+	}
+
+}
